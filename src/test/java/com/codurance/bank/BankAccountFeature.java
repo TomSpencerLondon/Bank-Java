36805376a7ee4ef88f49b95833c35edbd949c570
Tom <thomas.spencer@codurance.com>
2021-06-07 18:18:04 +0100

    Initial commit

---
+++ src/test/java/com/codurance/bank/BankAccountFeature.java
@@ -0,0 +1,19 @@
+package com.codurance.bank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountFeature {
+//  Date       || Amount || Balance
+//14/01/2012 || -500   || 2500
+//    13/01/2012 || 2000   || 3000
+//    10/01/2012 || 1000   || 1000
+
+  private AccountService service = new AccountService();
+
+
+  @Test
+  void prints_empty_statement() {
+
+  }
+}
